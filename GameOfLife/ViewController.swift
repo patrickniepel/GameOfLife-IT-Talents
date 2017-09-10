a788ec70cfb200611f29509dd7ea4072a8baccf9
patrickniepel <patrick.niepel@hof-unuiversity.de>
2017-09-10 14:39:59 +0200

    Initial Commit

---
+++ GameOfLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Patrick Niepel on 10.09.17.
+//  Copyright © 2017 Patrick Niepel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
